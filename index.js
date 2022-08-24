dcdc10bf4a81a5c02bcca48513b037613d5f550a
Peter <peterkilungya1@gmail.com>
2022-08-24 23:06:31 +0300

    assignment completeed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+// const main = document.getElementById("main");
+main.remove("main");
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Kevin is the champion";
+document.body.append(newHeader);
